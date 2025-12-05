ba26a2ee6931902796b15f038a23d6488adc7a27
Muhammad Mohsin <mmohsin.bscs25seecs@seecs.edu.pk>
2025-12-05 11:44:24 +0500

    Adding new feature

---
+++ new_feature.py
@@ -0,0 +1,6 @@
+def subtract(a,b):
+return print(f"The subtraction of "{a}" and "{b}"is: " {a-b})
+
+    a1 = float(input("Enter first number: "))
+    b1 = float(input("Enter second number which is subtracted: "))
+subtract(a1,b1)
